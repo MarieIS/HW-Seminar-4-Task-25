e953af933a892800a08b9e40d03038eed81bbc0d
Maria <mariya-ivanova@list.ru>
2024-03-30 14:03:39 +0200

    the solution is ready

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int GetNumber(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int baseNum = GetNumber("Insert a base: ");
+int powerNum = GetNumber("Insert a power: ");
+int multRes = 1;
+
+for (int i = 1; i <= powerNum; i++){
+   multRes *= baseNum;
+}
+
+Console.WriteLine($"{baseNum}, {powerNum} -> {multRes}");
